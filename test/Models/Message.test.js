f6ed90c45089e6b1860287216c261ecdafd20cf4
Josh Moncrieff <joshmoncrieff@me.com>
2017-02-12 16:09:48 +1100

    Move resolveInput method test to new Message model test file

--- test/Models/Message.test.js
+++ test/Models/Message.test.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const Message = require('../../models/Message');
+const kaomojiAsscLib = require('../../models/Kaomoji');
 // const fauxAsscLib = {
 //   happy: ['happy', 'glad', 'positive', 'yay'],
 //   sad: ['sad', 'unhappy', 'depressed', 'cry', 'crying', 'depressed'],
@@ -13,8 +14,23 @@ describe('Message', () => {
     expect(Message.getKaomojiList('happy').title).to.equal('happy');
   });
 
-  it('returns a kaomoji', () => {
+  it('returns a kaomoji from array of kaomoji', () => {
     const kaomoji = Message.chooseEmoji(fauxKaomoji);
     expect(fauxKaomoji.emoji.indexOf(kaomoji)).to.not.equal(-1);
+  });
+});
+
+describe('resolveInput', () => {
+  it('returns "happy" when associated text passed in', () => {
+    expect(Message.resolveInput('glad', kaomojiAsscLib)).to.equal('happy');
+  });
+
+  it('doesn\'t care about capitalization', () => {
+    expect(Message.resolveInput('Glad', kaomojiAsscLib)).to.equal('happy');
+    expect(Message.resolveInput('UNHAPPY', kaomojiAsscLib)).to.equal('sad');
+  });
+
+  it('returns "help" when no associated text is found', () => {
+    expect(Message.resolveInput('blerg', kaomojiAsscLib)).to.equal('help');
   });
 });
